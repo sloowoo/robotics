30fb7dc08fe173924f33d685fe894e9e7f051630
subatomic <70423273+subbo18@users.noreply.github.com>
2022-02-12 22:12:41 -0500

    Update main.cpp
    This is probably going to be the final competition code for my team. If it isn't, there would only be a few small tweaks. The code is all commented and explained.

--- src/main.cpp
+++ src/main.cpp
@@ -1,179 +1,378 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Thu Sep 26 2019                                           */
-/*    Description:  Clawbot Competition Template                              */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// Drivetrain           drivetrain    1, 10, D        
-// ClawMotor            motor         3               
-// ArmMotor             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-#include "vex.h"
 #include "string.h"
+#include "vex.h"
+#include <cmath>
+#include <ctime>
 #include <iostream>
 #include <vector>
-#include <ctime>
 
 using namespace vex;
+#define MAX 3
 
-#define MAX 4
+// motors
+// T/F checked
+motor frontLeftMotor = motor(PORT18, ratio18_1, false);
+motor frontRightMotor = motor(PORT7, ratio18_1, true);
+motor backLeftMotor = motor(PORT19, ratio18_1, false);
+motor backRightMotor = motor(PORT5, ratio18_1, true);
+motor BackLift1 = motor(PORT20, ratio18_1, true);
+motor ClawMotor = motor(PORT6, ratio18_1, false);
+motor BackLift2 = motor(PORT10, ratio18_1, false);
+motor LiftMotor = motor(PORT8, ratio18_1, false);
 
-// VEXcode device constructors
-motor frontLeftMotor = motor(PORT17, ratio18_1, false);
-motor frontRightMotor = motor(PORT10, ratio18_1, true);
-motor backLeftMotor = motor(PORT1, ratio18_1, false);
-motor backRightMotor = motor(PORT20, ratio18_1, true);
-motor LiftMotor1 = motor(PORT19, ratio18_1, true);   
-motor LiftMotor2 = motor(PORT8, ratio18_1, false);  
-motor ClawMotor = motor(PORT18, ratio18_1, false); 
-motor BackLift = motor(PORT7, ratio18_1, false); 
-
-//motor groups
+// motor groups
 motor_group leftDrive = motor_group(frontLeftMotor, backLeftMotor);
-motor_group rightDrive = motor_group(frontRightMotor,backRightMotor);
-motor_group liftMotors = motor_group(LiftMotor1, LiftMotor2);
+motor_group rightDrive = motor_group(frontRightMotor, backRightMotor);
+motor_group backLiftMotors = motor_group(BackLift1, BackLift2);
 
-//drivetrain
-// drivetrain Drivetrain = drivetrain(leftDrive, rightDrive, 345, 360, 340, mm, 1);
-//controller
+// controller
 controller Controller = controller();
-// VEXcode generated functions
+
 // A global instance of competition
 competition Competition;
 
-int count = 0;
+//axis4 will later be set to the Axis4 position of the joystick
+//because we will set a deadband for the Axis4 position
+//but you can't directly change the value of Controller.Axis4.position()
+//refer to the start of the usercontrol code
+int axis4 = 0;
 
-float perct = 0.6;
+//perct is a varible that is calibratable
+//it sets the velocity of the robot
+//refer to the drving code where the speed of the motor groups is multiplied by perct
+float perct = 0.55;
 
-void liftUp(){
-  liftMotors.spin(forward, 50, pct);
+//the reason we set two variables for the speed and turning
+//is so that the driving speed and turning speed doesn't have to be the same
+//makes it 2-dimensional and easily customizable
+float speedFactor = 1;
+float turnFactor = .5;
+
+void twerk() {
+  // a for loop to make the robot shake back and forth 2 times
+  for (int i = 0; i < 2; i++) {\
+    leftDrive.resetRotation();
+    rightDrive.resetRotation();
+
+    leftDrive.spinToPosition(80, deg, 200, rpm, false);
+    rightDrive.spinToPosition(80, deg, 200, rpm, true);
+
+    leftDrive.resetRotation();
+    rightDrive.resetRotation();
+
+    leftDrive.spinToPosition(-80, deg, 200, rpm, false);
+    rightDrive.spinToPosition(-80, deg, 200, rpm, true);
+  }
+  return;
+}
+
+
+
+//for the back lift
+void liftUp() {
+  LiftMotor.spin(forward, 50, pct);
   wait(25, msec);
   waitUntil(!Controller.ButtonL1.pressing());
-  liftMotors.stop();
+  LiftMotor.stop();
 }
 
-void liftDown(){
-  liftMotors.spin(reverse, 50, pct);
+void liftDown() {
+  LiftMotor.spin(reverse, 50, pct);
   wait(25, msec);
   waitUntil(!Controller.ButtonL2.pressing());
-  liftMotors.stop();
+  LiftMotor.stop();
 }
 
-void clawUp(){
-  ClawMotor.spin(reverse, 10, pct);
+//for the claw
+void clawUp() {
+  ClawMotor.spin(reverse, 20, pct);
   wait(25, msec);
   waitUntil(!Controller.ButtonR1.pressing());
   ClawMotor.stop();
 }
 
-void clawDown(){
-  ClawMotor.spin(forward, 10, pct);
+void clawDown() {
+  ClawMotor.spin(forward, 20, pct);
   wait(25, msec);
   waitUntil(!Controller.ButtonR2.pressing());
   ClawMotor.stop();
 }
 
+int deadband = 15;
+
+//used for the speedarr to loop through the array at lines around 300
+int b = 0;
+//the array of percentages you can set the robot's velocity at
+float speedarr[3] = {0.55, 0.40, 0.20};
+
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
 }
 
 void autonomous(void) {
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  backLiftMotors.resetRotation();
+  wait(25, msec);
 
-  
+  // simple
+  //goes forward while puttin the lift down
+  leftDrive.spinToPosition(-1369.83, deg, 125, rpm, false);
+  rightDrive.spinToPosition(-1369.83, deg, 125, rpm, false);
+  backLiftMotors.spinToPosition(1870, deg, 200, rpm, true);
+  wait(100, msec);
+  //lifts up
+  backLiftMotors.resetRotation();
+  backLiftMotors.spinToPosition(-675, deg, 200, rpm, true);
+  wait(100, msec);
+  //goes back
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(849.97, deg, 125, rpm, false);
+  rightDrive.spinToPosition(849.97, deg, 125, rpm, true);
+  wait(100, msec);
+  //puts lift down
+  backLiftMotors.resetRotation();
+  backLiftMotors.spinToPosition(675, deg, 200, rpm, true);
+  //goes down more
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(316.11, deg, 125, rpm, false);
+  rightDrive.spinToPosition(316.11, deg, 125, rpm, true);
+  wait(100, msec);
+  //turns a bit
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(170, deg, 125, rpm, false);
+  rightDrive.spinToPosition(-170, deg, 125, rpm, true);
+  //goes forward
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(-421.49, deg, 125, rpm, false);
+  rightDrive.spinToPosition(-421.49, deg, 125, rpm, true);
+  wait(100, msec);
+  //lift up
+  backLiftMotors.resetRotation();
+  backLiftMotors.spinToPosition(-675, deg, 200, rpm, true);
+  wait(100, msec);
+  //goes back
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(421.49, deg, 125, rpm, false);
+  rightDrive.spinToPosition(421.49, deg, 125, rpm, true);
+  //puts lift down
+  backLiftMotors.resetRotation();
+  backLiftMotors.spinToPosition(675, deg, 200, rpm, true);
+
+  // complicated
+  /*
+  //puts lift down and goes forward
+  backLiftMotors.spinToPosition(1870, deg, 200, rpm, false);
+  leftDrive.spinToPosition(-316.11, deg, 150, rpm, false);
+  rightDrive.spinToPosition(-316.11, deg, 150, rpm, true);
+  //turns a bit
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(40, deg, 150, rpm, false);
+  rightDrive.spinToPosition(-40, deg, 150, rpm, false);
+  wait(100, msec);
+  //goes forward a bit
+  leftDrive.resetRotation();
+  rightDrive.resetRotation();
+  leftDrive.spinToPosition(-849.97*1.25, deg, 150, rpm, false);
+  rightDrive.spinToPosition(-849.97*1.25, deg, 150, rpm, true);
+  //lifts up
+  backLiftMotors.resetRotation();
+  backLiftMotors.spinToPosition(-675, deg, 200, rpm, true);
+  wait(100, msec);
+  //goes back
+  leftDrive.spinToPosition(395.14, deg, 150, rpm, false);
+  rightDrive.spinToPosition(395.14, deg, 150, rpm, true);
+  //puts lift down
+  backLiftMotors.resetRotation();
+  backLiftMotors.spinToPosition(675, deg, 200, rpm, true);
+  */
+
+  wait(100, msec);
+  backLiftMotors.stop();
+  leftDrive.stop();
+  rightDrive.stop();
 }
 
-
-
-
 void usercontrol(void) {
-  // Deadband stops the motors when Axis values are close to zero.
-  int deadband = 15;
-
   while (true) {
-    
-    // Get the velocity percentage of the left motor. (Axis3 + Axis4)
-    int LeftDriveSpeed =
-        Controller.Axis3.position() + Controller.Axis4.position();
-    // Get the velocity percentage of the right motor. (Axis3 - Axis4)
-    int RightDriveSpeed =
-        Controller.Axis3.position() - Controller.Axis4.position();
+    float speed = Controller.Axis3.position();
+    axis4 = Controller.Axis4.position();
+    float turnn = axis4;
 
+    //set above
+    speed = speed * speedFactor;
+    //it's turn with two n's because turn is a pre built vex variable
+    turnn = turnn * turnFactor;
+
+    int LeftDriveSpeed = speed + turnn;
+    int RightDriveSpeed = speed - turnn;
     int BackLiftSpeed = Controller.Axis2.position();
-    // Set the speed of the left motor. If the value is less than the deadband,
-    // set it to zero.
+
     if (abs(LeftDriveSpeed) < deadband) {
-      // Set the speed to zero.
       leftDrive.setVelocity(0, percent);
-    } else {
-      // Set the speed to leftMotorSpeed
-      leftDrive.setVelocity(LeftDriveSpeed*perct, percent);
+    }
+    else {
+      //perct is set above as changable velocity
+      leftDrive.setVelocity(LeftDriveSpeed * perct, percent);
     }
 
-    // Set the speed of the right motor. If the value is less than the deadband,
-    // set it to zero.
     if (abs(RightDriveSpeed) < deadband) {
       rightDrive.setVelocity(0, percent);
-    } else {
-      rightDrive.setVelocity(RightDriveSpeed*perct, percent);
+    }
+    else {
+      rightDrive.setVelocity(RightDriveSpeed * perct, percent);
     }
 
+    //deadband for moving the joystick left and right so it doesn't turn
+    //if you just twitch a tiny bit
+    if (abs(Controller.Axis4.position()) < 2) {
+      axis4 = 0;
+    }
+    else {
+      axis4 = Controller.Axis4.position();
+    }
+    
     leftDrive.spin(forward);
     rightDrive.spin(forward);
 
+    backLiftMotors.setVelocity(BackLiftSpeed * 0.7, pct);
+    BackLift1.spin(reverse);
+    BackLift2.spin(reverse);
 
-    liftMotors.setStopping(hold);
+    LiftMotor.setStopping(hold);
     ClawMotor.setStopping(hold);
-    BackLift.setStopping(hold);
+    backLiftMotors.setStopping(hold);
 
-    // ClawMotor.setVelocity(50, pct);
-    //liftMotors.setVelocity(50, pct);
-    BackLift.setVelocity(BackLiftSpeed*0.7, pct);
-    BackLift.spin(forward);
+    //nudge right
+    while (Controller.ButtonY.pressing()) {
+      rightDrive.spin(forward, 10, pct);
+      leftDrive.spin(reverse, 10, pct);
+      wait(200, msec);
+      rightDrive.stop();
+      leftDrive.stop();
+    }
 
-    
+    //nudge left
+    while (Controller.ButtonA.pressing()) {
+      rightDrive.spin(reverse, 10, pct);
+      leftDrive.spin(forward, 10, pct);
+      wait(200, msec);
+      rightDrive.stop();
+      leftDrive.stop();
+    }
+
+    //goes straight back so it's easier to go up the ramp
+    while (Controller.ButtonB.pressing()) {
+      rightDrive.spin(reverse, 40, pct);
+      leftDrive.spin(reverse, 40, pct);
+      int BackLiftSpeed = Controller.Axis2.position();
+      backLiftMotors.setVelocity(BackLiftSpeed * 0.7, pct); 
+      backLiftMotors.setStopping(hold);
+      wait(50, msec);
+      rightDrive.stop();
+      leftDrive.stop();
+      
+    }
+
+
+    while (Controller.ButtonX.pressing()) {
+      //defined above
+      twerk();
+      wait(25, msec);
+    }
+
     Controller.ButtonL1.pressed(liftUp);
     Controller.ButtonL2.pressed(liftDown);
 
     Controller.ButtonR1.pressed(clawUp);
     Controller.ButtonR2.pressed(clawDown);
 
-    if (Controller.ButtonUp.pressing()) {
-      perct += 0.05;
-      wait(150, msec);
-    }
-    
-     if (Controller.ButtonDown.pressing()) {
-      perct -= 0.05;
-      wait(150, msec);
+
+    //b is used to loop through the speedarr[]
+    //and speedarr[] contains percentages which will be multiplied to the driving velocity
+    //so the driver can change the robot's speed in real time
+    if (Controller.ButtonDown.pressing()) {
+      if (b >= 2) {
+        wait(100, msec);
+        b = 0;
+      }
+      else {
+        wait(100, msec);
+        b += 1;
+      }
+      perct = speedarr[b];
     }
 
+    if (Controller.ButtonUp.pressing()) {
+      if (b <= 0) {
+        wait(100, msec);
+        b = 2;
+      }
+      else {
+        b -= 1;
+        wait(100, msec);
+      }
+      perct = speedarr[b];
+    }
+
+    //prints what percentage the driving speed is at
     Controller.Screen.clearScreen();
     Controller.Screen.setCursor(1, 1);
     Controller.Screen.print("     %.2f", perct);
-
     wait(25, msec);
   }
 }
 
 int main() {
-  // Set up callbacks for autonomous and driver control periods.
+  // Set up callbacks for autonomous and driver control periods
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
-  // Run the pre-autonomous function.
+  // Run the pre-autonomous function
   pre_auton();
 
-  // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
   }
 }
+
+   ///////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////
+  ////////////////                                                       /////
+///////////////                                                           /////
+//////////////                              amooga awooga                   ////
+/////////////                                                               ////
+//////////////                                                              ////
+///////////////                                                            ////
+////////////////                                                         /////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////                        ______                _____                 _____              
+////////////////////////////////////////////////////////////////////////////         |         |   |      |    |\    /|  /     \  |\    /|     /             /\     \   /
+/////////////////////////////////////////////////////////////////////////////        |         |   |______|    | \  / | |       | | \  / |    |             /  \     \ /
+//////////////////////////////////////////////////////////////////////////////       |         |   |  \        |  \/  | |       | |  \/  |    |    ___     /____\     |
+//////////////////////////////////////////////////////////////////////////////       |         |   |   \       |      | |       | |      |    |       |   /      \    |
+//////////////////////////////////////////////////////////////////////////////        \_______/    |    \      |      |  \_____/  |      |     \_____/   /        \   |
+//////////////////////////////////////////////////////////////////////////////         
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////          ///////////////////
+//////////////////////////////////////////////////////////////////////////////        ////////             //
+//////////////////////////////////////////////////////////////////////////////        ///////     awoo      //
+///////////////////////////////             /////////////////////////////////         ////////             //
+///////////////////////////////             ////////////////////////////////          //////////////////////
+///////////////////////////////             ////////////////////////////////          //////////////////////
+///////////////////////////////             ////////////////////////////////          //////////////////////
+///////////////////////////////             ///////////////////////////////            ////////////////////
+///////////////////////////////             ///////////////////////////////             ///////////////////
+ //////////////////////////////              ////////////////////////////               ////////  ////////
+  ///////////////////////////                  /////////////////////////                 //////    //////
+
